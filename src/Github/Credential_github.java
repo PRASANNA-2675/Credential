98d77defcdcd208276bdd24864e6c5073e9c251b
Admin <Admin@DESKTOP-0C050D6>
2020-01-11 12:27:03 +0530

    Design

---
+++ src/Github/Credential_github.java
@@ -0,0 +1,12 @@
+package Github;
+
+public class Credential_github {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("prasi18021995@gmail.com");
+		System.out.println("Github-s1u2m3s4-4340");
+	}
+
+}
